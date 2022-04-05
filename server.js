451811d2c0134b1ffec48303cc584e544a9633ab
joidk <95649301+joidk@users.noreply.github.com>
2022-04-05 19:52:19 +0100

    server.js

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const { app } =  require('./app');
-const dotenv = require('dotenv');
 
 const parses = require("./utils/parses");
 
-dotenv.config({ path: './config.env' });
-
 const PORT = parses.usablePort(+process.argv.slice(2));
+console.log(PORT);
 app.listen(PORT, (err) => {
   console.log((new Date()) + " Server listening to port ");
 });
