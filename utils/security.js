168b863bb758d4635fd4ae8f6dcfbf45388ae463
joidk <95649301+joidk@users.noreply.github.com>
2022-04-09 23:39:50 +0100

    Improve encrypt method

--- utils/security.js
+++ utils/security.js
@@ -1,29 +1,17 @@
 const crypto = require('crypto');
 const dotenv = require('dotenv');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 dotenv.config({ path: '../config.env' });
 
 
 exports.hash = function hash(password) {
-    return new Promise((resolve, reject) => {
-        const salt = crypto.randomBytes(8).toString("hex")
-
-        crypto.scrypt(password, salt, 64, (err, derivedKey) => {
-            if (err) reject(err);
-            resolve(salt + ":" + derivedKey.toString('hex'))
-        });
-    })
+    return bcrypt.hash(password, 12);
 }
 
 exports.verify = function verify(password, hash) {
-    return new Promise((resolve, reject) => {
-        const [salt, key] = hash.split(":")
-        crypto.scrypt(password, salt, 64, (err, derivedKey) => {
-            if (err) reject(err);
-            resolve(key == derivedKey.toString('hex'))
-        });
-    })
+    return bcrypt.compare(password,hash);
 }
 
 exports.createToken = function createToken(userId) {
