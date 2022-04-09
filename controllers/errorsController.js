981e903fbd678d38af2109f78b9541e844fbb9ed
joidk <95649301+joidk@users.noreply.github.com>
2022-04-09 15:13:19 +0100

    Handle uncaught exception and rejection

--- controllers/errorsController.js
+++ controllers/errorsController.js
@@ -2,19 +2,46 @@ const fs = require('fs');
 
 const jsonTemplate = JSON.parse(fs.readFileSync(`${__dirname}/../models/templates/jsonTemplate.json`));
 
-exports.errorsHandler = (err, req, res, next) => {
-    err.statusCode = err.statusCode || '500';
-    err.status = err.status || 'error';
-
+const sendErrorDev = (err, res) => {
     jsonResponse = JSON.parse(JSON.stringify(jsonTemplate));
     jsonResponse.data.kind = "error";
     jsonResponse.data.items = [{
         status: err.status,
-        message: err.message
+        error: err,
+        message: err.message,
+        stach: err.stack
     }];
+    return res.status(err.statusCode).json(jsonResponse);
+}
+
+const sendErrorProd = (err, res) => {
+    jsonResponse = JSON.parse(JSON.stringify(jsonTemplate));
+    jsonResponse.data.kind = "error";
+
+    if(err.isOperational) {
+        jsonResponse.data.items = [{
+            status: err.status,
+            message: err.message,
+        }];
+    }else {
+        jsonResponse.data.items = [{
+            status: err.status,
+            message: "The server exploded",
+        }];
+    }
+
+       return res.status(err.statusCode).json(jsonResponse);
+}
+
+exports.errorsHandler = (err, req, res, next) => {
+    err.statusCode = err.statusCode || '500';
+    err.status = err.status || 'error';
 
     if(!err.isOperational)
-        console.log(err.stack);
+        console.log(err);
 
-    return res.status(err.statusCode).json(jsonResponse)
+    if(process.env.NODE_ENV = "development")
+        return sendErrorDev(err,res);
+    else if(process.env.NODE_ENV = "production")
+        return sendErrorProd(err,res);
 }
--- server.js
+++ server.js
@@ -2,8 +2,22 @@ const { app } =  require('./app');
 
 const parses = require("./utils/parses");
 
+process.on("uncaughtException", err => {
+    console.log("UNCAUGHT EXCEPTION. Shutting down");
+    console.log(err.name, err.message);
+    process.exit(1);
+});
+
 const PORT = parses.usablePort(+process.argv.slice(2));
 console.log(PORT);
-app.listen(PORT, (err) => {
+const server = app.listen(PORT, (err) => {
   console.log((new Date()) + " Server listening to port ");
 });
+process.on("unhandledRejection", err => {
+    console.log(err.name, err.message);
+    console.log("UNHANDLED REJECTION. Shutting down");
+    server.close(() => {
+        process.exit(1);
+    });
+});
+
