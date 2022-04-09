47ec995a55135c5c0e136dbf817ad66148f94bc6
joidk <95649301+joidk@users.noreply.github.com>
2022-04-09 22:48:04 +0100

    Add queryConstructor

---
+++ db/queryConstructor.js
@@ -0,0 +1,44 @@
+const mysql = require('mysql2');
+
+const seperateParams = async (params) => {
+    if (typeof params !== 'object') {
+        throw new Error('Invalid input');
+    }
+
+    const keys = Object.keys(params);
+    const values = Object.values(params);
+
+    return {
+        keys,
+        values
+    }
+}
+exports.whereQuery = async (params) => {
+    const paramsArray = await seperateParams(params);
+    const string = " ?? = ?";
+
+    let sql = " WHERE" + mysql.format(string, paramsArray.keys[0]);
+    for(let i = 1; i < paramsArray.keys.length; ++i)
+        sql += " AND" + mysql.format(string, paramsArray.keys[i])
+    return { sql, values: paramsArray.values };
+}
+
+exports.insertQuery = async (params) => {
+    const paramsArray = await seperateParams(params);
+
+    let sql = "INSERT INTO ?? (" + mysql.format("??", paramsArray.keys[0]);
+    let sql2 = "VALUES (?";
+    for(let i = 1; i < paramsArray.keys.length; ++i) {
+        sql += mysql.format(",??", paramsArray.keys[i]);
+        sql2 += ",?";
+    }
+    sql += ") " + sql2 + ")";
+    return { sql, values: paramsArray.values };
+}
+
+exports.selectQuery = async (fields) => {
+    let sql = "SELECT " + mysql.format("??", fields[0]);
+    for(let i = 1; i < fields.length; ++i)
+        sql += mysql.format(",??", fields[i]);
+    return sql;
+}
--- models/usersModel.js
+++ models/usersModel.js
@@ -1,42 +1,22 @@
 
 const query = require("../db/dbConnection");
 
-const seperateParams = async (params) => {
-    if (typeof params !== 'object') {
-        throw new Error('Invalid input');
-    }
-
-    const keys = Object.keys(params);
-    const values = Object.values(params);
-
-    return {
-        keys,
-        values
-    }
-}
-
-const placeHolderFields = async (size) => {
-    let sqlString = "??";
-    for(let i = 1; i < size; i++){
-        sqlString += ",??"
-    }
-    return sqlString;
-}
+const { selectQuery } = require("../db/queryConstructor");
 
 const mysql = require('mysql2');
 class UserModel {
     tableName = "user";
 
     findById = async (fields, id) => {
-        const sqlFields = await placeHolderFields(fields.length);
-        const sqlQuery = mysql.format("SELECT " + sqlFields + " FROM ??", [...fields, this.tableName]);
-        return query(sqlQuery + " WHERE id = ?", [id]);
+        const sqlFields = await selectQuery(fields);
+        const sqlQuery = mysql.format( sqlFields + " FROM ??", [this.tableName]);
+        return await query(sqlQuery + " WHERE id = ?", [id]);
     }
 
     findByPhone = async (fields, internationalNumber) => {
-        const sqlFields = await placeHolderFields(fields.length);
-        const sqlQuery = mysql.format("SELECT " + sqlFields + " FROM ??", [...fields, this.tableName]);
-        return query(sqlQuery + " WHERE internationalNumber = ?", [internationalNumber]);
+        const sqlFields = await selectQuery(fields);
+        const sqlQuery = mysql.format(sqlFields + " FROM ??", [this.tableName]);
+        return await query(sqlQuery + " WHERE internationalNumber = ?", [internationalNumber]);
     }
 }
 
